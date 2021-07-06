9f88954cf6f73f645b4d6cdedb614e9974d33115
eggbean <815311+eggbean@users.noreply.github.com>
2021-07-06 03:21:51 +0100

    Add bin package and rename dotfiles stow script

---
+++ stow-bin.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## Usage: ./stow-bin.sh
+
+set -e
+
+pushd /home/"$(logname)"/.dotfiles >/dev/null
+
+bind=(bin/*)
+for b in "${bind[@]}"; do
+	[ -e /usr/local/bin/"$(basename $b)" ] && (rm /usr/local/bin/"$(basename $b)" && echo "Existing /usr/local/bin/$(basename $b) deleted")
+done
+stow -Svt /usr/local/bin bin || (echo "Error stowing bin package" >&2 && exit 1)
+echo "DONE:  bin package stowed"
+
+exit
