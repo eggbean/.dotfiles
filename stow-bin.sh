3b66a4c0bc65c3331e1cc8ac30edd1b9b72bb715
eggbean <815311+eggbean@users.noreply.github.com>
2021-07-22 12:45:57 +0100

    Temporary fix for Stow v2.3.1 - redirect stderr and filter it

--- stow-bin.sh
+++ stow-bin.sh
@@ -17,16 +17,16 @@ bind=("${arch}"/*)
 for b in "${bind[@]}"; do
 	[ -e /usr/local/bin/"$(basename "$b")" ] && (rm /usr/local/bin/"$(basename "$b")" && echo "Existing /usr/local/bin/$(basename "$b") deleted")
 done
-stow -Rvt /usr/local/bin "${arch}" && echo "DONE: bin package stowed" || (echo "ERROR stowing bin package" >&2 && exit 1)
+stow -Rvt /usr/local/bin "${arch}" 2>&1 | grep -v "BUG in find_stowed_path" && echo "DONE: bin package stowed" || (echo "ERROR stowing bin package" >&2 && exit 1)
 popd >/dev/null
 
 pushd .man >/dev/null
 mand=(*)
 for m in "${mand[@]}"; do
-	stow -Rvt /usr/share/man/"$m" "$m"
+	stow -Rvt /usr/share/man/"$m" "$m" 2>&1 | grep -v "BUG in find_stowed_path"
 done && echo "DONE: man package stowed" || (echo "ERROR stowing man package" >&2 && exit 1)
 popd >/dev/null
 
-stow -Rvt /etc/bash_completion.d .completions && echo "DONE: bash completions package stowed" || (echo "ERROR stowing bash completions package" >&2 && exit 1)
+stow -Rvt /etc/bash_completion.d .completions 2>&1 | grep -v "BUG in find_stowed_path" && echo "DONE: bash completions package stowed" || (echo "ERROR stowing bash completions package" >&2 && exit 1)
 
 exit
