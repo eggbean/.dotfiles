670f985fc09fc6b759fb69fa22e5e4d1bffb57c6
eggbean <815311+eggbean@users.noreply.github.com>
2022-03-07 02:34:09 +0000

    Add alacritty config for Windows and change background colour to match nordfox nvim theme

--- bin/scripts/dotfiles2appdata.sh
+++ bin/scripts/dotfiles2appdata.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-[[ "$(id -u)" = "0" ]] && { echo "This script is not supposed to be run as root" >&2; exit 1; }
-
 cp -r ~/.dotfiles/config/.config/qutebrowser/ /mnt/c/Users/$USER/AppData/Roaming/qutebrowser/
 cp ~/.dotfiles/config/.config/vim/vimrc /mnt/c/Users/$USER/vimfiles/vimrc
 cp ~/.dotfiles/config/.config/vim/colors/summerfruit256.vim /mnt/c/Users/jason/vimfiles/colors/
 cp ~/.dotfiles/config/.config/vim/gvimrc /mnt/c/Users/$USER/vimfiles/gvimrc
 cp ~/.dotfiles/config/.config/mpv/input.conf /mnt/c/Users/$USER/AppData/Roaming/mpv/
 cp ~/.dotfiles/config/.config/alacritty/alacritty.yml.windows /mnt/c/Users/$USER/AppData/Roaming/alacritty/alacritty.yml
+cp ~/.dotfiles/config/.config/alacritty/alacritty.main.yml /mnt/c/Users/$USER/AppData/Roaming/alacritty/alacritty.main.yml
