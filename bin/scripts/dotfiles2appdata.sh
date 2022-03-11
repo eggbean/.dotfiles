8a4756e7c268d90e9446bdb150d144ab22d1408a
eggbean <815311+eggbean@users.noreply.github.com>
2022-03-11 04:03:42 +0000

    Add way of changing rifle.conf for Windows without needing a separate branch

--- bin/scripts/dotfiles2appdata.sh
+++ bin/scripts/dotfiles2appdata.sh
@@ -4,8 +4,8 @@
 [ ! -d /mnt/c/Users/$USER/AppData/Roaming/qutebrowser/config/ ] && mkdir -p /mnt/c/Users/$USER/AppData/Roaming/qutebrowser/config/
 cp -r ~/.dotfiles/config/.config/qutebrowser/* /mnt/c/Users/$USER/AppData/Roaming/qutebrowser/config/
 
-# gvim
-[ ! -d /mnt/c/Users/$USER/vimfiles/ ] && mkdir /mnt/c/Users/$USER/vimfiles/
+# gvim (files made read-only to avoid me accidentally editing them in Windows)
+[ ! -d /mnt/c/Users/$USER/vimfiles/vimrc.d/ ] && mkdir -p /mnt/c/Users/$USER/vimfiles/vimrc.d/
 cd /mnt/c/Users/$USER/vimfiles/
 attrib.exe -R vimrc
 attrib.exe -R gvimrc
@@ -23,3 +23,10 @@ cp ~/.dotfiles/config/.config/mpv/* /mnt/c/Users/$USER/AppData/Roaming/mpv/
 [ ! -d /mnt/c/Users/$USER/AppData/Roaming/alacritty/ ] && mkdir /mnt/c/Users/$USER/AppData/Roaming/alacritty/
 cp ~/.dotfiles/config/.config/alacritty/alacritty.yml.windows /mnt/c/Users/$USER/AppData/Roaming/alacritty/alacritty.yml
 cp ~/.dotfiles/config/.config/alacritty/alacritty.main.yml /mnt/c/Users/$USER/AppData/Roaming/alacritty/alacritty.main.yml
+
+# ranger/rifle
+if [ ! -L ~/.dotfiles/config/.config/ranger/rifle.conf ]; then
+  rm ~/.dotfiles/config/.config/ranger/rifle.conf
+  ln -s ~/.dotfiles/config/.config/ranger/rifle.conf.windows ~/.dotfiles/config/.config/ranger/rifle.conf
+  git update-index --skip-worktree ~/.dotfiles/config/.config/ranger/rifle.conf
+fi
