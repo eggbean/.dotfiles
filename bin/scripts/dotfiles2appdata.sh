1a542889479ece268a77092d401badea5b403605
eggbean <815311+eggbean@users.noreply.github.com>
2022-04-13 06:34:37 +0100

    Fix unquoted variables issue (minor)

--- bin/scripts/dotfiles2appdata.sh
+++ bin/scripts/dotfiles2appdata.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 # qutebrowser
-[ ! -d /mnt/c/Users/$USER/AppData/Roaming/qutebrowser/config/ ] && mkdir -p /mnt/c/Users/$USER/AppData/Roaming/qutebrowser/config/
-cp -r ~/.dotfiles/config/.config/qutebrowser/* /mnt/c/Users/$USER/AppData/Roaming/qutebrowser/config/
+[ ! -d /mnt/c/Users/"$USER"/AppData/Roaming/qutebrowser/config/ ] && mkdir -p /mnt/c/Users/"$USER"/AppData/Roaming/qutebrowser/config/
+cp -r ~/.dotfiles/config/.config/qutebrowser/* /mnt/c/Users/"$USER"/AppData/Roaming/qutebrowser/config/
 
 # gvim (files made read-only to avoid me accidentally editing them in Windows)
-[ ! -d /mnt/c/Users/$USER/vimfiles/vimrc.d/ ] && mkdir -p /mnt/c/Users/$USER/vimfiles/vimrc.d/
-cd /mnt/c/Users/$USER/vimfiles/
+[ ! -d /mnt/c/Users/"$USER"/vimfiles/vimrc.d/ ] && mkdir -p /mnt/c/Users/"$USER"/vimfiles/vimrc.d/
+cd /mnt/c/Users/"$USER"/vimfiles/ || { echo "ERROR: /mnt/c/Users/$USER/vimfiles/ not found." >&2; exit 1; }
 attrib.exe -R vimrc
 attrib.exe -R gvimrc
 attrib.exe -R vimrc.d\\*
-cp -r ~/.dotfiles/config/.config/vim/* /mnt/c/Users/$USER/vimfiles/
+cp -r ~/.dotfiles/config/.config/vim/* .
 attrib.exe +R vimrc
 attrib.exe +R gvimrc
 attrib.exe +R vimrc.d\\*
 
 # mpv
-[ ! -d /mnt/c/Users/$USER/AppData/Roaming/mpv/ ] && mkdir /mnt/c/Users/$USER/AppData/Roaming/mpv/
-cp ~/.dotfiles/config/.config/mpv/* /mnt/c/Users/$USER/AppData/Roaming/mpv/
+[ ! -d /mnt/c/Users/"$USER"/AppData/Roaming/mpv/ ] && mkdir /mnt/c/Users/"$USER"/AppData/Roaming/mpv/
+cp ~/.dotfiles/config/.config/mpv/* /mnt/c/Users/"$USER"/AppData/Roaming/mpv/
 
 # alacritty
-[ ! -d /mnt/c/Users/$USER/AppData/Roaming/alacritty/ ] && mkdir /mnt/c/Users/$USER/AppData/Roaming/alacritty/
-cp ~/.dotfiles/config/.config/alacritty/alacritty.yml.windows /mnt/c/Users/$USER/AppData/Roaming/alacritty/alacritty.yml
-cp ~/.dotfiles/config/.config/alacritty/alacritty.main.yml /mnt/c/Users/$USER/AppData/Roaming/alacritty/alacritty.main.yml
+[ ! -d /mnt/c/Users/"$USER"/AppData/Roaming/alacritty/ ] && mkdir /mnt/c/Users/"$USER"/AppData/Roaming/alacritty/
+cp ~/.dotfiles/config/.config/alacritty/alacritty.yml.windows /mnt/c/Users/"$USER"/AppData/Roaming/alacritty/alacritty.yml
+cp ~/.dotfiles/config/.config/alacritty/alacritty.main.yml /mnt/c/Users/"$USER"/AppData/Roaming/alacritty/alacritty.main.yml
 
 # ranger/rifle
 if [ ! -L ~/.dotfiles/config/.config/ranger/rifle.conf ]; then
