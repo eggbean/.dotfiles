e3dabe0adb4eb6e6187f96f3bdca1dcecbc91dbd
eggbean <815311+eggbean@users.noreply.github.com>
2022-07-17 13:44:18 +0100

    Add apt-repo-github.sh for GitHub CLI

---
+++ bin/scripts/apt-repo-github.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$(id -u)" -ne "0" ]; then { echo "This script must be run as root." >&2; exit 1; }; fi
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+apt update
+apt install gh
