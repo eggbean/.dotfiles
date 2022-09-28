8b12afe8a7a80af350777f88f3acf958b0c2a988
eggbean <815311+eggbean@users.noreply.github.com>
2022-09-28 03:31:55 +0100

    Update tmux configuration with scratch shell popup and cleanup
    The statusbar styling is still not fixed

--- config/.config/tmux/scripts/session-alert.sh
+++ config/.config/tmux/scripts/session-alert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Changes status-left colour when an alert triggers on a non-current session
+# Changes a variable when an alert triggers on a non-current session, which can be used to style the statusline
 
 session_id=$( tmux display-message -p -F "#{session_id}" )
 alert=$( tmux list-sessions -F '#{session_id} #{session_alerts}' | grep -v ^$session_id | awk '/[^ ]$/ {print}' | wc -l )
