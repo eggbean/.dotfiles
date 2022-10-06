1c6f66c443d7b47e60bbbed83692127ca2479cd7
eggbean <815311+eggbean@users.noreply.github.com>
2022-10-06 04:02:53 +0100

    Set $WIN_HOME environment variable and use in dotfiles2appdata.sh

--- bin/scripts/dotfiles2appdata.sh
+++ bin/scripts/dotfiles2appdata.sh
@@ -2,19 +2,24 @@
 
 # This script essentially copies Linux configuration files from the dotfiles repository to Windows
 
+[[ "$(id -u)" = "0" ]] && { echo "This script is not supposed to be run as root" >&2; exit 1; }
+
+WIN_HOME_RAW="$(cmd.exe /c "<nul set /p=%UserProfile%" 2>/dev/null)"
+WIN_HOME="$(wslpath "$WIN_HOME_RAW")"
+
 # qutebrowser
-[ ! -d /mnt/c/Users/"$USER"/AppData/Roaming/qutebrowser/config ] \
-  && mkdir -p /mnt/c/Users/"$USER"/AppData/Roaming/qutebrowser/config
-cp -r ~/.dotfiles/config/.config/qutebrowser/* /mnt/c/Users/"$USER"/AppData/Roaming/qutebrowser/config
+[ ! -d "$WIN_HOME"/AppData/Roaming/qutebrowser/config ] \
+  && mkdir -p "$WIN_HOME"/AppData/Roaming/qutebrowser/config
+cp -r "$HOME"/.dotfiles/config/.config/qutebrowser/* "$WIN_HOME"/AppData/Roaming/qutebrowser/config
 
 # vim/gvim
 # added words from Windows vim spelling file added to the local spelling file in repository
 # config files made read-only to avoid me accidentally editing them in Windows
-cd /mnt/c/Users/"$USER" || { echo "ERROR" >&2; exit 1; }
+cd "$WIN_HOME" || { echo "ERROR" >&2; exit 1; }
 [ ! -d vimfiles/vimrc.d ] && mkdir -p vimfiles/vimrc.d && attrib.exe +H vimfiles
 cd vimfiles || { echo "ERROR" >&2; exit 1; }
 winspell="spell/en.utf-8.add"
-localspell="$HOME/.dotfiles/config/.config/vim/spell/en.utf-8.add"
+localspell="$HOME"/.dotfiles/config/.config/vim/spell/en.utf-8.add
 if [ -e "$winspell" ]; then
   comm -1 -3 <(sort -u "$localspell") <(sort -u "$winspell") > /tmp/difference
   cat "$localspell" /tmp/difference | awk "!a[\$0]++{print}" > /tmp/concatenated
@@ -28,7 +33,7 @@ files=('vimrc' 'gvimrc' 'vimrc.d/autocmds.vim' 'vimrc.d/common.vim' 'vimrc.d/dis
 for f in "${files[@]}"; do
   if [ -f "$f" ]; then attrib.exe -R "$f" >/dev/null; fi
 done
-cp -r ~/.dotfiles/config/.config/vim/{vimrc,gvimrc} .
+cp -r "$HOME"/.dotfiles/config/.config/vim/{vimrc,gvimrc} .
 vimdirs=('after' 'autoload' 'colors' 'compiler' 'doc' 'ftdetect' 'ftplugin' \
   'indent' 'keymap' 'plugin' 'spell' 'syntax' 'templates' 'UltiSnips' 'vimrc.d')
 for d in "${vimdirs[@]}"; do
@@ -41,13 +46,13 @@ for f in "${files[@]}"; do
 done
 
 # mpv
-[ ! -d /mnt/c/Users/"$USER"/AppData/Roaming/mpv ] \
-  && mkdir /mnt/c/Users/"$USER"/AppData/Roaming/mpv
-cp -r ~/.dotfiles/config/.config/mpv/* /mnt/c/Users/"$USER"/AppData/Roaming/mpv
+[ ! -d "$WIN_HOME"/AppData/Roaming/mpv ] \
+  && mkdir "$WIN_HOME"/AppData/Roaming/mpv
+cp -r "$HOME"/.dotfiles/config/.config/mpv/* "$WIN_HOME"/AppData/Roaming/mpv
 
 # ranger
 # rifle.conf file swapped over with one that works with Windows and change ignored by git
-cd ~/.dotfiles || { echo "ERROR" >&2; exit 1; }
+cd "$HOME"/.dotfiles || { echo "ERROR" >&2; exit 1; }
 if [ ! -L config/.config/ranger/rifle.conf ]; then
   rm config/.config/ranger/rifle.conf
   ln -s config/.config/ranger/rifle.conf.windows config/.config/ranger/rifle.conf
@@ -56,19 +61,19 @@ fi
 
 # alacritty
 # the Windows base file is renamed when copied to %APPDATA%
-[ ! -d /mnt/c/Users/"$USER"/AppData/Roaming/alacritty ] \
-  && mkdir /mnt/c/Users/"$USER"/AppData/Roaming/alacritty
-cp ~/.dotfiles/config/.config/alacritty/alacritty.windows.yml /mnt/c/Users/"$USER"/AppData/Roaming/alacritty/alacritty.yml
-cp ~/.dotfiles/config/.config/alacritty/alacritty.main.yml /mnt/c/Users/"$USER"/AppData/Roaming/alacritty/alacritty.main.yml
+[ ! -d "$WIN_HOME"/AppData/Roaming/alacritty ] \
+  && mkdir "$WIN_HOME"/AppData/Roaming/alacritty
+cp "$HOME"/.dotfiles/config/.config/alacritty/alacritty.windows.yml "$WIN_HOME"/AppData/Roaming/alacritty/alacritty.yml
+cp "$HOME"/.dotfiles/config/.config/alacritty/alacritty.main.yml "$WIN_HOME"/AppData/Roaming/alacritty/alacritty.main.yml
 
 # Windows Terminal
 # includes hacky way to get a copy of any changed settings back to the repository
-if [ -d /mnt/c/Users/"$USER"/winfiles/Windows_Terminal ]; then
-  [ ! -d /mnt/c/Users/"$USER"/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState ] \
-    && mkdir -p /mnt/c/Users/"$USER"/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState
-  cp /mnt/c/Users/"$USER"/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json \
-    /mnt/c/Users/"$USER"/winfiles/Windows_Terminal/settings.json.bak
-  unix2dos -q /mnt/c/Users/"$USER"/winfiles/Windows_Terminal/settings.json.bak
-  cp /mnt/c/Users/"$USER"/winfiles/Windows_Terminal/settings.json \
-    /mnt/c/Users/"$USER"/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState
+if [ -d "$WIN_HOME"/winfiles/Windows_Terminal ]; then
+  [ ! -d "$WIN_HOME"/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState ] \
+    && mkdir -p "$WIN_HOME"/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState
+  cp "$WIN_HOME"/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json \
+    "$WIN_HOME"/winfiles/Windows_Terminal/settings.json.bak
+  unix2dos -q "$WIN_HOME"/winfiles/Windows_Terminal/settings.json.bak
+  cp "$WIN_HOME"/winfiles/Windows_Terminal/settings.json \
+    "$WIN_HOME"/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState
 fi
