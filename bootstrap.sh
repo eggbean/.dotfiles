fa11c877a50f7c5e244bf6e6771a4f1f46f3b13f
eggbean <815311+eggbean@users.noreply.github.com>
2022-11-05 19:04:33 +0000

    Move bootstrap.sh scripts to scripts directory so that it's in $PATH

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
-
-cd ~/.dotfiles || (echo "Cannot find ~/.dotfiles" >&2; exit 1)
-
-# Stow binaries
-if [ "$UID" -ne 0 ] && [ "$EUID" -ne 0 ]; then
-  sudo bin/scripts/stow-bin.sh
-else
-  bin/scripts/stow-bin.sh --nosudo
-fi
-# Stow dotfiles
-bin/scripts/stow-dotfiles.sh config
-
-source ~/.bash_profile
+bin/scripts/bootstrap.sh
