10aad63996d16d3ae99e0d9f3a8affad21fc7ae5
eggbean <815311+eggbean@users.noreply.github.com>
2022-11-10 07:31:10 +0000

    Rename repo adding scripts to make them easier to remember
    ...and remove apt-repo-charm.sh and their repository is a mess and I
    don't really need it.

--- bin/scripts/apt-repo-charm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ "$(id -u)" -ne "0" ]; then { echo "This script must be run as root." >&2; exit 1; }; fi
-
-echo 'deb [trusted=yes] https://repo.charm.sh/apt/ /' | tee /etc/apt/sources.list.d/charm.list > /dev/null
-apt update
