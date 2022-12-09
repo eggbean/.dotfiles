e1e2fad4c687e0148dd28188d58dfad7cb6aa595
eggbean <815311+eggbean@users.noreply.github.com>
2022-12-09 00:44:50 +0000

    Add script to install docker on centos

---
+++ bin/scripts/install_docker_centos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+starttime="$EPOCHSECONDS"
+
+if [ "$(id -u)" -ne "0" ]; then { echo "This script must be run as root." >&2; exit 1; }; fi
+
+dnf install -y dnf-plugins-core
+dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+dnf install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+usermod -aG docker "$(logname)"
+systemctl enable docker.service
+systemctl enable containerd.service
+systemctl start docker
+docker run hello-world
+
+seconds="$(( "${EPOCHSECONDS}" - "${starttime}" ))"
+printf 'Time taken %d:%02d:%02d\n' "$((seconds/3600))" "$(((seconds/60)%60))" "$((seconds%60))"
