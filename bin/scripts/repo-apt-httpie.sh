3cce3f69da60a502a601fff3f2b7d61c31b1286a
eggbean <815311+eggbean@users.noreply.github.com>
2023-01-27 06:26:34 +0000

    Add repo-apt-httpie.sh
    httpie can be installed from the epel repository on RHEL distros.

---
+++ bin/scripts/repo-apt-httpie.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$(id -u)" -ne "0" ]; then { echo "This script must be run as root." >&2; exit 1; }; fi
+
+# Install httpie
+curl -SsL https://packages.httpie.io/deb/KEY.gpg | apt-key add -
+curl -SsL -o /etc/apt/sources.list.d/httpie.list https://packages.httpie.io/deb/httpie.list
+apt update
+apt install httpie
