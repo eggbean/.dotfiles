34c866a2ef1da4752d8791e69d8181ad904fc6d7
eggbean <815311+eggbean@users.noreply.github.com>
2023-03-16 00:15:51 +0000

    Minor fix: change apt to apt-get as apt shouldn't be used in scripts

--- bin/scripts/repo-apt-httpie.sh
+++ bin/scripts/repo-apt-httpie.sh
@@ -5,5 +5,5 @@ if [ "$(id -u)" -ne "0" ]; then { echo "This script must be run as root." >&2; e
 # Install httpie
 curl -SsL https://packages.httpie.io/deb/KEY.gpg | apt-key add -
 curl -SsL -o /etc/apt/sources.list.d/httpie.list https://packages.httpie.io/deb/httpie.list
-apt update
-apt install httpie
+apt-get update
+apt-get install httpie
