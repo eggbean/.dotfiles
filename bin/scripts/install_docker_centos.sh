53fb7a0f044ef378610224464dcf5534c619c6e0
eggbean <815311+eggbean@users.noreply.github.com>
2023-05-03 03:35:46 +0100

    Modify docker installation script to update zsh completion

--- bin/scripts/install_docker_centos.sh
+++ bin/scripts/install_docker_centos.sh
@@ -2,7 +2,7 @@
 
 starttime="$EPOCHSECONDS"
 
-if [ "$(id -u)" -ne "0" ]; then { echo "This script must be run as root." >&2; exit 1; }; fi
+if [[ $(id -u) -ne 0 ]]; then { echo "This script must be run as root." >&2; exit 1; }; fi
 
 dnf install -y dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
@@ -13,5 +13,8 @@ systemctl enable containerd.service
 systemctl start docker
 docker run hello-world
 
-seconds="$(( "${EPOCHSECONDS}" - "${starttime}" ))"
-printf 'Time taken %d:%02d:%02d\n' "$((seconds/3600))" "$(((seconds/60)%60))" "$((seconds%60))"
+wget -q https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker -O /home/jason/.dotfiles/bin/zsh-completions/_docker
+chown jason:jason /home/jason/.dotfiles/bin/zsh-completions/_docker
+
+seconds=$(( EPOCHSECONDS - starttime ))
+printf 'Time taken %d:%02d:%02d\n' $((seconds/3600)) $(((seconds/60)%60)) $((seconds%60))
