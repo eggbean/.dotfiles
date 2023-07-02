cd474175ad79f0579db8c49e69ee2e94030d230b
eggbean <815311+eggbean@users.noreply.github.com>
2023-07-02 03:16:22 +0100

    Improve stow scripts
    Use tee to show output in STDOUT as well as make log files. Bringing back
    STDOUT output is useful for when the scripts are executed individually.
    Now the bootstrap uses gum to avoid the noise that it was previously
    disabled for.

--- bin/scripts/stow-bin.sh
+++ bin/scripts/stow-bin.sh
@@ -28,8 +28,8 @@ while true; do
   shift
 done
 
-if [ "$(id -u)" = "0" ]; then
-  if [ -n "$SUDO_USER" ]; then
+if [[ $(id -u) = 0 ]]; then
+  if [[ -n $SUDO_USER ]]; then
     USER="$SUDO_USER"
   else
     USER="$(logname)"
@@ -41,27 +41,25 @@ fi
 STOW_DIR="$HOME/.dotfiles/bin"
 
 # Set variables for (re)stowing or unstowing
-if [ -n "$unstow" ]; then
+if [[ -n $unstow ]]; then
   stowcom='-D'
   stowed='unstowed'
-  rm "$XDG_STATE_HOME"/binaries_stowed
 else
   stowcom='-R'
   stowed='stowed'
-  if [ ! -d "$XDG_STATE_HOME" ]; then mkdir -p "$XDG_STATE_HOME"; fi
-  touch "$XDG_STATE_HOME"/binaries_stowed
 fi
 
 # Set variables for target locations and make directories if necessary
-if [ -n "$nosudo" ]; then
+if [[ -n $nosudo ]]; then
   targetdir="$HOME/.local/bin"
   mandir="$HOME/.local/share/man"
   bashcompdir="$HOME/.local/share/bash-completion/completions"
   zshcompdir="$HOME/.local/share/zsh/site-functions"
   fontdir="$HOME/.local/share/fonts"
 else
-  if [ "$(id -u)" -ne "0" ]; then
-    echo "This script must be run as root to stow in /usr, or use the --nosudo option to stow in ~/.local." >&2
+  if [[ $(id -u) -ne 0 ]]; then
+    echo "This script must be run as root to stow in /usr,\
+ or use the --nosudo option to stow in ~/.local." >&2
     exit 1
   fi
   targetdir='/usr/local/bin'
@@ -72,65 +70,85 @@ else
 fi
 
 # If stowing, make target directories if they don't exist
-if [ -z "$unstow" ]; then
+if [[ -z $unstow ]]; then
   for d in "$targetdir" "$mandir" "$bashcompdir" "$zshcompdir" "$fontdir"; do
-    if [ ! -d "$d" ]; then mkdir -p "$d"; fi
+    if [[ ! -d $d ]]; then mkdir -p "$d"; fi
   done
-  if [ -d "$HOME/.dotfiles/bin/man" ]; then
+  if [[ -d $HOME/.dotfiles/bin/man ]]; then
     pushd "$HOME/.dotfiles/bin/man" >/dev/null
     mansubs=(*) && popd >/dev/null
     for s in "${mansubs[@]}"; do
-      if [ ! -d "$mandir"/"$s" ]; then mkdir "$mandir"/"$s"; fi
+      if [[ ! -d $mandir/$s ]]; then mkdir "${mandir}/${s}"; fi
     done
   fi
 fi
 
 # Use the correct binaries for CPU architecture
-if [ "$(uname -o)" = "Android" ]; then
-  [ "$(dpkg --print-architecture)" = "aarch64" ] && arch='android' || { echo "CPU architecture unknown" >&2; exit 1; }
+if [[ $(uname -o) == Android ]]; then
+  [[ $(dpkg --print-architecture) == aarch64 ]] && arch='android' || \
+    { echo "CPU architecture unknown" >&2; exit 1; }
 else
-  [ "$(arch)" = "armv7l" ] && arch='armv7l'
-  [ "$(arch)" = "aarch64" ] && arch='aarch64'
-  [ "$(arch)" = "x86_64" ] && arch='x86_64'
-  [ -z "$arch" ] && { echo "CPU architecture unknown" >&2; exit 1; }
+  [[ $(arch) == armv7l ]] && arch='armv7l'
+  [[ $(arch) == aarch64 ]] && arch='aarch64'
+  [[ $(arch) == x86_64 ]] && arch='x86_64'
+  [[ -z $arch ]] && { echo "CPU architecture unknown" >&2; exit 1; }
 fi
 
 pushd "$STOW_DIR" >/dev/null
 
+if [[ -e ${TMPDIR:-/tmp}/stow-bin.sh.log ]]; then
+  rm ${TMPDIR:-/tmp}/stow-bin.sh.log
+fi
+
 # Stow/unstow binaries
-stow $stowcom -vt "$targetdir" "$arch" 2>"${TMPDIR:-/tmp}"/stow-bin.sh.log
+stow $stowcom -vt "$targetdir" "$arch" \
+  2> >(tee -a ${TMPDIR:-/tmp}/stow-bin.sh.log >&2)
 
 # Stow/unstow shell scripts
-stow $stowcom -vt "$targetdir" scripts 2>>"${TMPDIR:-/tmp}"/stow-bin.sh.log
+stow $stowcom -vt "$targetdir" scripts \
+  2> >(tee -a ${TMPDIR:-/tmp}/stow-bin.sh.log >&2)
 
 # Stow/unstow man files
 pushd "$STOW_DIR/man" >/dev/null
 mansubs=(*)
 for m in "${mansubs[@]}"; do
-  stow $stowcom -vt "$mandir"/"$m" "$m" 2>>"${TMPDIR:-/tmp}"/stow-bin.sh.log
+  stow $stowcom -vt "$mandir"/"$m" "$m" \
+    2> >(tee -a ${TMPDIR:-/tmp}/stow-bin.sh.log >&2)
 done
 popd >/dev/null
 
 # Stow/unstow bash completion files
-stow $stowcom -vt "$bashcompdir" bash-completions 2>>"${TMPDIR:-/tmp}"/stow-bin.sh.log
+stow $stowcom -vt "$bashcompdir" bash-completions \
+  2> >(tee -a ${TMPDIR:-/tmp}/stow-bin.sh.log >&2)
 
 # Stow/unstow zsh completion files
-stow $stowcom -vt "$zshcompdir" zsh-completions 2>>"${TMPDIR:-/tmp}"/stow-bin.sh.log
+stow $stowcom -vt "$zshcompdir" zsh-completions \
+  2> >(tee -a ${TMPDIR:-/tmp}/stow-bin.sh.log >&2)
 
 # Stow/unstow fonts if local desktop system, but not on WSL
-if [ -n "$DISPLAY" ] && grep -vqi microsoft /proc/version; then
-  stow --no-folding $stowcom -vt "$fontdir" fonts 2>>"${TMPDIR:-/tmp}"/stow-bin.sh.log
+if [[ -n $DISPLAY ]] && grep -vqi microsoft /proc/version; then
+  stow --no-folding $stowcom -vt "$fontdir" fonts \
+    2> >(tee -a ${TMPDIR:-/tmp}/stow-bin.sh.log >&2)
   fc-cache -f
 fi
 
+# Create/remove state marker
+if [[ -n $unstow ]]; then
+  rm "$XDG_STATE_HOME"/binaries_stowed
+else
+  if [[ ! -d $XDG_STATE_HOME ]]; then mkdir -p "$XDG_STATE_HOME"; fi
+  touch "$XDG_STATE_HOME"/binaries_stowed
+fi
+
 # Clean up any empty directories when unstowing
-if [ -n "$unstow" ]; then
+if [[ -n $unstow ]]; then
   pushd man >/dev/null
   mansubs=(*) && popd >/dev/null
     for r in "${mansubs[@]}"; do
-      if [ -d "$mandir"/"$r" ]; then rmdir --ignore-fail-on-non-empty "$mandir"/"$r"; fi
+      if [[ -d $mandir/$r ]]; then
+        rmdir --ignore-fail-on-non-empty "${mandir}/${r}"; fi
     done
   for e in "$targetdir" "$mandir" "$bashcompdir" "$zshcompdir" "$fontdir"; do
-    if [ -d "$e" ]; then rmdir -p --ignore-fail-on-non-empty "$e"; fi
+    if [[ -d $e ]]; then rmdir -p --ignore-fail-on-non-empty "$e"; fi
   done
 fi
--- bin/scripts/stow-dotfiles.sh
+++ bin/scripts/stow-dotfiles.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
-[ "$(id -u)" = "0" ] && { echo "This script is not supposed to be run as root" >&2; exit 1; }
+[ "$(id -u)" = "0" ] && \
+  { echo "This script is not supposed to be run as root" >&2; exit 1; }
 
 # Make some dummy files to prevent stow folding
 [ ! -d ~/.config ] && mkdir ~/.config
@@ -15,10 +16,18 @@ pushd ~ >/dev/null
 shellfiles=( .bash_aliases .bash_login .bash_logout .bash_profile .bashrc .inputrc )
 for file in "${shellfiles[@]}"; do
   if [ -e "$file" ] && [ ! -L "$file" ]; then
-    if [ ! -d "${TMPDIR:-/tmp}/existing_files" ]; then mkdir "${TMPDIR:-/tmp}/existing_files"; fi
+    if [ ! -d "${TMPDIR:-/tmp}/existing_files" ]; then
+      mkdir "${TMPDIR:-/tmp}/existing_files"
+    fi
     mv "$file" "${TMPDIR:-/tmp}/existing_files"
   fi
 done
 
 pushd ~/.dotfiles >/dev/null
-stow --adopt -Rv -d ~/.dotfiles -t ~ config 2>"${TMPDIR:-/tmp}"/stow-dotfiles.sh.log
+
+if [[ -e ${TMPDIR:-/tmp}/stow-dotfiles.sh.log ]]; then
+  rm ${TMPDIR:-/tmp}/stow-dotfiles.sh.log
+fi
+
+stow --adopt -Rv -d ~/.dotfiles -t ~ config \
+  2> >(tee -a ${TMPDIR:-/tmp}/stow-dotfiles.sh.log >&2)
