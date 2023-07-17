5522282c6a2bee9bbc3a8389ef549721f5a9490b
eggbean <815311+eggbean@users.noreply.github.com>
2023-07-17 11:25:26 +0100

    Add colour reply to kernel update check

--- bin/scripts/kernel-update-check.sh
+++ bin/scripts/kernel-update-check.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+yelw=$(tput setaf 3)
+norm=$(tput sgr0)
+
 cd /boot || exit 1
 shopt -s nullglob ; for file in config-* ; do kernels+=( "${file#config-}" ) ; done
 newest="$(printf '%s\n' "${kernels[@]}" | sort -V -t - -k 1,2 | tail -n1)"
 current="$(uname -r)"
-[[ $current != $newest ]] && echo "Reboot needed for new kernel"
+[[ $current != $newest ]] && echo "${yelw}Reboot needed for new kernel${norm}"
