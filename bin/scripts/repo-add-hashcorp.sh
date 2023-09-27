57aeb48d0ecb5b14211b2fbc08c3c6119856a70a
eggbean <815311+eggbean@users.noreply.github.com>
2023-09-27 05:05:16 +0100

    Update Hashicorp repo script to work on Fedora

--- bin/scripts/repo-add-hashcorp.sh
+++ bin/scripts/repo-add-hashcorp.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 if [[ $(id -u) != 0 ]]; then
   { echo "This script must be run as root." >&2; exit 1; }
@@ -10,21 +10,27 @@ fi
 if [[ -z $XDG_STATE_HOME ]]; then
   export XDG_STATE_HOME="/home/$USER/.local/state"
 fi
-[[ -e $XDG_STATE_HOME/hashicorp_repo_installed ]] && exit 1
+[[ -e $XDG_STATE_HOME/hashicorp_repo_installed ]] && exit 125
 
 eval "$(source /etc/os-release && typeset -p ID)"
-if [[ $ID =~ ^(rhel|fedora.*|amzn|ol|rocky)$ ]]; then
+if [[ $ID =~ ^(rhel|fedora.*|amzn|ol|rocky|almalinux)$ ]]; then
+  if [[ $ID =~ ^(rhel|ol|rocky|almalinux)$ ]]; then release=RHEL; fi
+  if [[ $ID =~ ^(fedora.*)$ ]]; then release=fedora; fi
+  if [[ $ID == amzn ]]; then release=AmazonLinux; fi
+  if [[ -z $release ]]; then { echo ERROR >&2; exit 22; }; fi
   { dnf install -y dnf-plugins-core && \
-    dnf config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo; } || \
+    dnf config-manager --add-repo https://rpm.releases.hashicorp.com/$release/hashicorp.repo; } || \
     { echo Adding Hashicorp repository failed >&2; exit 1; }
-elif [[ $ID =~ ^(debian|ubuntu|pop|raspbian)$ ]]; then
+elif [[ $ID =~ ^(debian|ubuntu|pop|linuxmint|raspbian)$ ]]; then
   { apt-get update && apt-get install -y gnupg software-properties-common curl && \
-    curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add - && \
-    sh -c "printf 'deb\t[arch=amd64]\thttps://apt.releases.hashicorp.com\t$(lsb_release -cs)\tmain\n' \
-    > /etc/apt/sources.list.d/hashicorp.list"; } || \
+    wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor > \
+    /usr/share/keyrings/hashicorp-archive-keyring.gpg && \
+    sh -c "printf 'deb\t[signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg]\t \
+      https://apt.releases.hashicorp.com\t$(lsb_release -cs)\tmain\n' > \
+      /etc/apt/sources.list.d/hashicorp.list"; } || \
     { echo Adding Hashicorp repository failed >&2; exit 1; }
 else
-    { echo OS not supported by Hashicorp repository installation script >&2; exit 1; }
+    { echo OS not supported by Hashicorp repository installation script >&2; exit 95; }
 fi
 
 touch $XDG_STATE_HOME/hashicorp_repo_installed
