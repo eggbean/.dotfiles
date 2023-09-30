4e5523549b0e341156be1b1982bf4611420ee7f6
eggbean <815311+eggbean@users.noreply.github.com>
2023-09-30 15:20:26 +0100

    Update exa-wrapper.sh to eza

--- bin/scripts/eza-wrapper.sh
+++ bin/scripts/eza-wrapper.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# This is a modified version of my exa-wrapper.sh script (https://bit.ly/exa-wrapper)
+# This is a modified version of my eza-wrapper.sh script (https://bit.ly/eza-wrapper)
 # which has a couple of conditional tweaks for use on Windows Subsystem for Linux
 
-## Change following to '0' for output to be like ls and '1' for exa features
+## Change following to '0' for output to be like ls and '1' for eza features
 # Don't list implied . and .. by default with -a
 dot=0
 # Show human readable file sizes by default
@@ -17,7 +17,7 @@ lnk=0
 # Show file git status automatically (can cause a slight delay in large repo trees)
 git=1     # Replacing with test for variable set by direnv
           # as git for Linux is very slow on NTFS
-          if [[ $no_exa_git =~ true ]]; then git=0; else git=1; fi
+          if [[ $no_eza_git =~ true ]]; then git=0; else git=1; fi
 # Show icons
 ico=0
 # Show column headers
@@ -68,30 +68,30 @@ EOF
 
 [[ $* =~ --help ]] && help
 
-exa_opts=()
+eza_opts=()
 
 while getopts ':aAbtucSI:rkhnsXL:MNg1lFGRdDioTx@' arg; do
   case $arg in
-    a) (( dot == 1 )) && exa_opts+=(-a) || exa_opts+=(-a -a) ;;
-    A) exa_opts+=(-a) ;;
-    t) exa_opts+=(-s modified); ((++rev)) ;;
-    u) exa_opts+=(-us accessed); ((++rev)) ;;
-    c) exa_opts+=(-Us created); ((++rev)) ;;
-    S) exa_opts+=(-s size); ((++rev)) ;;
-    I) exa_opts+=(--ignore-glob="${OPTARG}") ;;
+    a) (( dot == 1 )) && eza_opts+=(-a) || eza_opts+=(-a -a) ;;
+    A) eza_opts+=(-a) ;;
+    t) eza_opts+=(-s modified); ((++rev)) ;;
+    u) eza_opts+=(-us accessed); ((++rev)) ;;
+    c) eza_opts+=(-Us created); ((++rev)) ;;
+    S) eza_opts+=(-s size); ((++rev)) ;;
+    I) eza_opts+=(--ignore-glob="${OPTARG}") ;;
     r) ((++rev)) ;;
     k) ((--hru)) ;;
     h) ((++hru)) ;;
-    n) exa_opts+=(--git-ignore) ;;
-    s) exa_opts+=(-S) ;;
-    X) exa_opts+=(-s extension) ;;
-    L) exa_opts+=(--level="${OPTARG}") ;;
-    o) exa_opts+=(--octal-permissions) ;;
+    n) eza_opts+=(--git-ignore) ;;
+    s) eza_opts+=(-S) ;;
+    X) eza_opts+=(-s extension) ;;
+    L) eza_opts+=(--level="${OPTARG}") ;;
+    o) eza_opts+=(--octal-permissions) ;;
     M) ((++gpd)) ;;
     N) ((++nco)) ;;
     g) ((++git)) ;;
     b) ((--meb)) ;;
-    1|l|F|G|R|d|D|i|T|x|@) exa_opts+=(-"$arg") ;;
+    1|l|F|G|R|d|D|i|T|x|@) eza_opts+=(-"$arg") ;;
     :) printf "%s: -%s switch requires a value\n" "${0##*/}" "${OPTARG}" >&2; exit 1
        ;;
     *) printf "Error: %s\n       --help for help\n" "${0##*/}" >&2; exit 1
@@ -101,28 +101,28 @@ done
 
 shift "$((OPTIND - 1))"
 
-(( rev == 1 )) && exa_opts+=(-r)
-(( fgp == 0 )) && exa_opts+=(-g)
-(( lnk == 0 )) && exa_opts+=(-H)
-(( hru <= 0 )) && exa_opts+=(-B)
-(( hed == 1 )) && exa_opts+=(-h)
-(( meb == 0 && hru > 0 )) && exa_opts+=(-b)
-(( col == 1 )) && exa_opts+=(--color=always) || exa_opts+=(--color=auto)
-(( nco == 1 )) && exa_opts+=(--color=never)
-(( gpd >= 1 )) && exa_opts+=(--group-directories-first)
-(( ico == 1 )) && exa_opts+=(--icons)
+(( rev == 1 )) && eza_opts+=(-r)
+(( fgp == 0 )) && eza_opts+=(-g)
+(( lnk == 0 )) && eza_opts+=(-H)
+(( hru <= 0 )) && eza_opts+=(-B)
+(( hed == 1 )) && eza_opts+=(-h)
+(( meb == 0 && hru > 0 )) && eza_opts+=(-b)
+(( col == 1 )) && eza_opts+=(--color=always) || eza_opts+=(--color=auto)
+(( nco == 1 )) && eza_opts+=(--color=never)
+(( gpd >= 1 )) && eza_opts+=(--group-directories-first)
+(( ico == 1 )) && eza_opts+=(--icons)
 (( git == 1 )) && \
-  [[ $(git -C ${*:-.} rev-parse --is-inside-work-tree) == true ]] 2>/dev/null && exa_opts+=(--git)
+  [[ $(git -C ${*:-.} rev-parse --is-inside-work-tree) == true ]] 2>/dev/null && eza_opts+=(--git)
 
 # Test for variable set by direnv to ignore desktop.ini
 # and registry files on NTFS shared partitions
-[[ $exa_ignore =~ true ]] && exa_opts+=(-I "desktop.ini|ntuser.*|NTUSER.*")
+[[ $eza_ignore =~ true ]] && eza_opts+=(-I "desktop.ini|ntuser.*|NTUSER.*")
 
-# Use my patched version of exa on NTFS drives so that files
+# Use my patched version of eza on NTFS drives so that files
 # aren't all seen as executables, so appear in $LS_COLORS.
 if grep -qi microsoft /proc/version; then
   shopt -s extglob
-  [[ $(realpath ${*:-.}) == /@(mnt|?)/* ]] 2>/dev/null && exabin=exa-ntfs || exabin=eza
+  [[ $(realpath ${*:-.}) == /@(mnt|?)/* ]] 2>/dev/null && ezabin=eza-ntfs || ezabin=eza
 fi
 
-${exabin:-eza} --color-scale "${exa_opts[@]}" "$@"
+${ezabin:-eza} --color-scale "${eza_opts[@]}" "$@"
